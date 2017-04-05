d781c06f7fd786625b622f09fee22905c3d93290
Steven Rodriguez <sgrod91@gmail.com>
2017-04-05 15:57:55 -0400

    Added Express Middleware

--- restaurant_reviewer.js
+++ restaurant_reviewer.js
@@ -7,6 +7,7 @@ const pgp = require('pg-promise')({
 });
 const bodyParser = require('body-parser');
 const app = express();
+const session = require('express-session');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -15,8 +16,60 @@ const db = pgp({
   database: 'restaurant_db_2'
 });
 
+app.use(session({
+  secret: 'steelism',
+  cookie: {
+    maxAge: 60000000
+  }
+}));
+
+app.use(function(req, resp, next) {
+  resp.locals.session = req.session;
+  next();
+});
+
+app.get('/login', function(req, resp) {
+  resp.render('login.hbs');
+});
+
+app.post('/submit_login', function(req, resp) {
+  var username = req.body.username;
+  var password = req.body.password;
+  db.one(`select * from reviewer where name = $1 and password = $2`, [username, password])
+  .then(function() {
+    req.session.loggedInUser = username;
+    resp.redirect('/');
+  })
+  .catch(function(err) {
+    resp.redirect('/login');
+  });
+});
+//   if (username === 'Steven' && password === 'sgrod91') {
+//     //succesfull login
+//     req.session.loggedInUser = username;
+//     resp.redirect('/');
+//   } else {
+//     resp.redirect('/login');
+//   }
+// });
+
+app.use(function authentication(req, resp, next) {
+  if (req.session.loggedInUser) {
+    next();
+  } else {
+    resp.redirect('/login');
+  }
+});
+
 app.get('/', function(req, resp) {
-  resp.render('index.hbs');
+  resp.render('index.hbs', {
+    name: req.session.loggedInUser
+  });
+});
+
+app.get('/logout', function(req, res) {
+  req.session.loggedInUser = null;
+  res.redirect('/login');
 });
 
 app.get('/search_results', function(req, resp, next) {

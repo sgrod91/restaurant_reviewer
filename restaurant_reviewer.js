139595bae28d20655304e83a47ccd02be3da4190
Steven Rodriguez <sgrod91@gmail.com>
2017-04-04 21:45:14 -0400

    Restaurant review app using express, javaScript, html, css, mySQL

--- restaurant_reviewer.js
+++ restaurant_reviewer.js
@@ -1,47 +1,85 @@
 /* jshint esversion: 6 */
 
-var express = require("express");
-var Promise = require("bluebird");
-var bodyParser = require("body-parser");
-var app = express();
-
-const pgp = require('pg-promise') ({
+const express = require('express');
+const Promise = require('bluebird');
+const pgp = require('pg-promise')({
   promiseLib: Promise
 });
-
-var db = pgp({database: 'restaurant_db_2'});
-
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-app.get('/', function(request, response) {
-  response.render('index.hbs');
+const db = pgp({
+  database: 'restaurant_db_2'
 });
 
-app.get('/search_results', function(request, response) {
-  let term = request.query.searchTerm;
+app.get('/', function(req, resp) {
+  resp.render('index.hbs');
+});
+
+app.get('/search_results', function(req, resp, next) {
+  let term = req.query.searchTerm;
   console.log('Term:', term);
-  db.any(`select * from restaurant where name ilike '%${term}%'`)
+  db.any(`
+    select * from restaurant
+    where restaurant.name ilike '%${term}%'
+    `)
     .then(function(resultsArray) {
-      console.log('search', resultsArray);
-      response.render('search_results.hbs', {
-        search_results: resultsArray
+      console.log('results', resultsArray);
+      resp.render('search_results.hbs', {
+        results: resultsArray
       });
-    });
+    })
+    .catch(next);
 });
 
-app.get('/restaurant/:id', function(request, response) {
-  var id = request.params.id;
-  console.log(id);
-  db.one(`select * from restaurant where id = ${id}`)
-    .then(function(restaurant) {
-      response.render('restaurant.hbs', {
-        restaurant: restaurant
+app.get('/restaurant/:id', function(req, resp, next) {
+  let id = req.params.id;
+  db.any(`
+    select
+      reviewer.name as reviewer_name,
+      review.title,
+      review.stars,
+      review.review
+    from
+      restaurant
+    inner join
+      review on review.restaurant_id = restaurant.id
+    left outer join
+      reviewer on review.reviewer_id = reviewer.id
+    where restaurant.id = ${id}
+  `)
+    .then(function(reviews) {
+      return [
+        reviews,
+        db.one(`
+          select name as restaurant_name, * from restaurant
+          where id = ${id}`)
+      ];
+    })
+    .spread(function(reviews, restaurant) {
+      resp.render('restaurant.hbs', {
+        restaurant: restaurant,
+        reviews: reviews
       });
-    });
+    })
+    .catch(next);
+});
+
+app.post('/write_review/:id', function(req, resp, next) {
+  var restaurantId = req.params.id;
+  console.log('restaurant ID', restaurantId);
+  console.log('from the form', req.body);
+  db.none(`insert into review values
+    (default, NULL, ${req.body.stars}, '${req.body.title}', '${req.body.review}', ${restaurantId})`)
+    .then(function() {
+      resp.redirect(`/restaurant/${restaurantId}`);
+    })
+    .catch(next);
 });
 
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+  console.log('Listening on port 3000.');
 });
